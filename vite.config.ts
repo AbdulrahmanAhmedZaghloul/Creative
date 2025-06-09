278019656fb811fdba982a93eae8f34a22fb81ed
AbdulrahmanAhmedZaghloul <abdozaghloul990@gmail.com>
2025-06-09 16:15:13 +0300

    company-2

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: '0.0.0.0'
+  },
+  base: './' // 🔥 مهم لروابط الملفات والصور تشتغل صح على Netlify
 });
